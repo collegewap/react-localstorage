4097a05a4616b55f78c92db991f0ee33cd9e328b
Abhishek EH <collegewap@gmail.com>
2021-06-13 12:05:54 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card, FormGroup, InputGroup } from "@blueprintjs/core";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card elevation="1">
+        <form onSubmit={formSubmitHandler}>
+          <FormGroup label="Name" labelFor="name">
+            <InputGroup
+              id="Name"
+              placeholder="Name"
+              type="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
+          <Button intent="primary" text="Submit" fill type="submit" />
+        </form>
+      </Card>
     </div>
   );
 }
